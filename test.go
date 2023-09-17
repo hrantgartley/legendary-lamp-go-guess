cbb9915951cc3a0b68376411527e47b48bd47d5f
Grant <grant@Grants-MacBook-Pro.local>
2023-09-17 15:06:52 -0500

    timed
    good

--- test.go
+++ test.go
@@ -7,14 +7,23 @@ import (
 
 func main() {
 	welcome()
-	number := generateNumber(100)
+	threshold := userRange()
+	rand.New(rand.NewSource(int64(threshold)))
+	number := generateNumber(threshold)
 	guess := 0
 	for guess != number {
-		fmt.Print("Guess a number between 1 and " + fmt.Sprint(number) + ": ")
+		fmt.Print("Guess a number between 1 and " + fmt.Sprint(threshold) + ": ")
 		fmt.Scan(&guess)
 		giveHint(guess, number)
 	}
-	goodye()
+	goodbye() // Corrected the function name
+}
+
+func userRange() int {
+	var rangeNumber int
+	fmt.Print("Enter a guess threshold: ")
+	fmt.Scan(&rangeNumber)
+	return rangeNumber
 }
 
 func giveHint(number int, target int) {
@@ -28,10 +37,10 @@ func giveHint(number int, target int) {
 }
 
 func generateNumber(number int) int {
-	return rand.Intn(number)
+	return rand.Intn(number) + 1 // Adjusted to generate numbers between 1 and the threshold
 }
 
-func goodye() {
+func goodbye() { // Corrected the function name
 	fmt.Println("Thank you for playing my guessing game")
 }
 
